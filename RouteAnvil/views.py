912bf7dc4e73fcad0f97e915b26e12bf9acc7b54
IkherVee <ik.villaseca@duocuc.cl>
2025-09-05 17:34:19 -0400

    links a paginas

--- RouteAnvil/views.py
+++ RouteAnvil/views.py
@@ -6,21 +6,21 @@ def index(request):
 
 
 def choferes(request):
-    return render(request, 'index.html')
+    return render(request, 'choferes.html')
 
 def vehiculos(request):
-    return render(request, 'index.html')
+    return render(request, 'vehiculos.html')
 
 def pasajeros(request):
-    return render(request, 'index.html')
+    return render(request, 'pasajeros.html')
 
 def rutas(request):
-    return render(request, 'index.html')
+    return render(request, 'rutas.html')
 
 def destinos(request):
-    return render(request, 'index.html')
+    return render(request, 'destinos.html')
 
 
 
 def generar_ruta(request):
-    return render(request, 'index.html')
+    return render(request, 'generar_ruta.html')
