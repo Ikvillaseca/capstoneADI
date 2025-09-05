5679a6b148d8e7ac608afbdc524bb3f599786274
IkherVee <ik.villaseca@duocuc.cl>
2025-09-05 17:04:03 -0400

    urls base

--- RouteAnvil/urls.py
+++ RouteAnvil/urls.py
@@ -4,5 +4,10 @@ from RouteAnvil import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('choferes/', views.choferes, name='choferes'),
+    path('vehiculos/', views.vehiculos, name='vehiculos'),
+    path('pasajeros/', views.pasajeros, name='pasajeros'),
+    path('rutas/', views.rutas, name='rutas'),
+    path('generar_ruta/', views.generar_ruta, name='generar_ruta'),
 
 ]
--- RouteAnvil/views.py
+++ RouteAnvil/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render
 
-# Create your views here.
+#Aqui definimos cada vista de django (backend en python)
 def index(request):
+    return render(request, 'index.html')
+
+
+def choferes(request):
+    return render(request, 'index.html')
+
+def vehiculos(request):
+    return render(request, 'index.html')
+
+def pasajeros(request):
+    return render(request, 'index.html')
+
+def rutas(request):
+    return render(request, 'index.html')
+
+
+def generar_ruta(request):
     return render(request, 'index.html')
