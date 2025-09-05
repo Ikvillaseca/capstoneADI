6e03c7503f1488ea5fa6e7b9a94f58098e903cac
IkherVee <ik.villaseca@duocuc.cl>
2025-09-05 18:14:28 -0400

    modelos agregados a admin

--- RouteAnvil/admin.py
+++ RouteAnvil/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Chofer, Destino, Origen, Pasajero, Reserva, Vehiculo, Viaje
 
 # Register your models here.
+
+#Modelos de RouteAnvil
+admin.site.register(Chofer)
+admin.site.register(Destino)
+admin.site.register(Origen)
+admin.site.register(Pasajero)
+admin.site.register(Reserva)
+admin.site.register(Vehiculo)
+admin.site.register(Viaje)
