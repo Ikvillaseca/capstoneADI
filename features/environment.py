0ed044f65a08f73a2ec221f2db7593c49717392b
IkherVee <ik.villaseca@duocuc.cl>
2025-09-11 16:09:50 -0300

    fix id_pasajeros

---
+++ features/environment.py
@@ -0,0 +1,12 @@
+from behave import fixture, use_fixture
+from selenium.webdriver import Firefox
+
+@fixture
+def browser_firefox(context):
+    context.browser = Firefox()
+    yield context.browser
+    # -- CLEANUP-FIXTURE PART:
+    context.browser.quit()
+
+def before_all(context):
+    use_fixture(browser_firefox, context)
