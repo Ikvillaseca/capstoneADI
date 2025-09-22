1f2c2d2e86fadfa7d4f66a67ad63377588f43188
IkherVee <ik.villaseca@duocuc.cl>
2025-09-22 17:48:27 -0300

    Implementacion base de pruebas en behave con selenium

--- features/environment.py
+++ features/environment.py
@@ -1,12 +1,21 @@
 from behave import fixture, use_fixture
-from selenium.webdriver import Firefox
+from selenium.webdriver import Firefox,Chrome
 
 @fixture
-def browser_firefox(context):
-    context.browser = Firefox()
+def browser_chrome(context):
+    context.browser = Chrome()
     yield context.browser
-    # -- CLEANUP-FIXTURE PART:
     context.browser.quit()
 
 def before_all(context):
-    use_fixture(browser_firefox, context)
+    use_fixture(browser_chrome, context)
+
+
+#== Pruebas en Firefox
+# def browser_firefox(context):
+#     context.browser = Firefox()
+#     yield context.browser
+#     context.browser.quit()
+
+# def before_all(context):
+#     use_fixture(browser_firefox, context)
