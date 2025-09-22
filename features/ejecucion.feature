8268c3883bef19d03f28a1c89eac74ed9ff3028c
IkherVee <ik.villaseca@duocuc.cl>
2025-09-22 17:59:29 -0300

    Mejor documentacion de las pruebas

--- features/ejecucion.feature
+++ features/ejecucion.feature
@@ -1,7 +1,7 @@
 Feature: Ejecucion del servidor de Django
-Para las pruebas se debe verificar que se tiene acceso a la url de Django
+  Para las pruebas se debe verificar que se tiene acceso a la url de Django
 
-    Scenario: Navegar a la pagina de inicio
-        Given El navegador esta abierto
-        When Navegue a la pagina de inicio
-        Then Observare que accedi a la pagina con titulo "RouteAnvil" 
+  Scenario: Navegar a la pagina de inicio
+    Given El navegador esta abierto
+    When Navegue a la pagina de inicio
+    Then Observare que accedi a la pagina con titulo "RouteAnvil" 
--- features/environment.py
+++ features/environment.py
@@ -12,18 +12,22 @@ def before_all(context):
     use_fixture(browser_chrome, context)
 
 def get_url(context, path=""):
-
+    #Intento obtener la url desde el entorno, 
+    #O valor de la url al ejecutar "behave -D base_url="
+    #O sino desde context
+    #O sino la url por defecto de django
     base_url = (
         context.config.userdata.get('base_url') or 
         getenv('BASE_URL') or 
         getattr(context, 'base_url', None) or
         'http://127.0.0.1:8000'
     )
-    """Helper para construir URLs completas"""
+    #Helper para construir URLs completas
     if path.startswith('/'):
-        return f"{base_url}{path}"
-    return f"{base_url}/{path}"
-
+        url = f"{base_url}{path}"
+    else:
+        url = f"{base_url}/{path}"
+    return url
 
 #== Pruebas en Firefox
 # def browser_firefox(context):
