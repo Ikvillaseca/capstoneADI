3992f6c1746b0ab2f15d26177cc3064ee6637602
Alonzo <alonzotobarmartinez@gmail.com>
2025-09-25 17:09:49 -0300

    Validaciones formularios de pasajeros

--- RouteAnvil/forms.py
+++ RouteAnvil/forms.py
@@ -71,8 +71,45 @@ class FormularioPasajero(forms.ModelForm):
         model = Pasajero
         fields = ['rut', 'nombre', 'apellido', 'telefono', 'empresa_trabajo']
 
+        
+    def clean_rut(self):
+        rut = self.cleaned_data['rut']
+        if not re.match(r'^\d{7,8}-[\dkK]$', rut):
+            raise forms.ValidationError("El RUT debe tener el siguiente formato (ejemplo: 12345678-9).")
+        return rut
+    
+
+    def clean_nombre(self):
+        nombre = self.cleaned_data['nombre']
+        if not nombre.isalpha():
+            raise forms.ValidationError("El nombre solo debe contener letras.")
+        return nombre
+
+
+    def clean_apellido(self):
+        apellido = self.cleaned_data['apellido']
+        if not apellido.isalpha():
+            raise forms.ValidationError("El apellido solo debe contener letras.")
+        return apellido
+    
+    def clean_telefono(self):
+        telefono = self.cleaned_data['telefono']
+        if not re.match(r'^\+?1?\d{9,15}$', telefono):
+            raise forms.ValidationError("El numero de telefono debe tener entre 9 y 15 digitos y puede incluir el codigo de pais.")
+        return telefono
+    
+    def clean_empresa_trabajo(self):
+        empresa_trabajo = self.cleaned_data['empresa_trabajo']
+        if len(empresa_trabajo) < 3:
+            raise forms.ValidationError("El nombre de la empresa debe tener al menos 3 caracteres.")
+        return empresa_trabajo
+
+
+
 #Formulario para poder crear un vehiculo de forma simple con los datos que definimos en el modelo
 class VehiculoForm(forms.ModelForm):
     class Meta:
         model = Vehiculo
         fields = ['id_vehiculo', 'patente', 'marca', 'capacidad', 'estado', 'revision_tecnica', 'proxima_revision']
+    
+    
